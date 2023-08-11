edb0ae75a565ddf536ead5e1c349aa1b4d597b85
Sergiu Butu <sergiu.butu@3ss.tv>
2023-08-11 13:01:58 +0300

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+
+const root = document.getElementById('root');
+if (root) {
+    ReactDOM.createRoot(root).render(<App />);
+}
